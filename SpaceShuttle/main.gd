3221ba61e1ae9e464428184aea208fb7b4007f4b
Joseph W. Linton II <91572148+Darkhero789@users.noreply.github.com>
2024-10-26 15:34:11 -0600

    Art, Json, & Movement

--- SpaceShuttle/main.gd
+++ SpaceShuttle/main.gd
@@ -1,10 +1,14 @@
 extends Node2D
-
+var rng = RandomNumberGenerator.new()
+var RI
+var dif
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	if dif == "Easy":
+		RI = rng.randi_range(0,10)
+	if dif == "Med":
+		RI = rng.randi_range(11,20)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
