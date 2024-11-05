a88867648768077787264aa274c40a83ae56493a
Joseph Gregory <91172241+Datramer@users.noreply.github.com>
2024-11-05 16:32:53 -0700

    fixed mouse accelerating movement, and allowed for up and left or right movement

--- SpaceShuttle/player.gd
+++ SpaceShuttle/player.gd
@@ -9,16 +9,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.y += .98
-	if position.y >= 560:
-		destroy_rocket()
-
-func _unhandled_input(event):
 	if Input.is_action_pressed("W"):
 		position.y -= 4
-	elif Input.is_action_pressed("A"):
+	if Input.is_action_pressed("A"):
 		position.x -= 2
 	elif Input.is_action_pressed("D"):
 		position.x += 2
+	if position.y >= 560:
+		destroy_rocket()
+	
 
 func destroy_rocket():
 	queue_free()
