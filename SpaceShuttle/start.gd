371c8c93b799eb15688c91e495dce07044fb2163
colinmbowser <83146931+colinmbowser@users.noreply.github.com>
2024-11-06 15:28:03 -0700

    added buttons
    added difficulty buttons
    created functions for buttons
    connected the buttons to functions
    created 2d Sprites for temporary backgrounds

--- SpaceShuttle/start.gd
+++ SpaceShuttle/start.gd
@@ -6,6 +6,7 @@ var rng = RandomNumberGenerator
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hide_difficulty_buttons()
 	pass # Replace with function body.
 
 
@@ -16,25 +17,60 @@ func _process(delta: float) -> void:
 func Play():
 	pass
 
-func _on_startButton_pressed():
-	pc = player.instantiate()
-	pc.position = Vector2(20,20)
-	add_child(pc)
-	hide_buttons()
+func _on_start_button_pressed():
+	hide_stc_buttons()
+	show_diffculty_buttons()
 
-func _on_tutorialButton_pressed():
+func _on_tutorial_button_pressed():
+	hide_stc_buttons()
 	pass
 
-func _on_creditsButton_pressed():
+func _on_credits_button_pressed():
+	hide_stc_buttons()
 	pass
 
-func _on_settingsButton_pressed():
-	pass
+func _on_music_button_pressed():
+	pass # Replace with function body.
 
-func hide_buttons():
+func _on_audio_button_pressed():
+	pass # Replace with function body.
+
+func _on_easy_button_pressed():
+	hide_difficulty_buttons()
+	tempLander()
+
+func _on_medium_button_pressed():
+	hide_difficulty_buttons()
+	tempLander()
+
+func _on_hard_button_pressed():
+	hide_difficulty_buttons()
+	tempLander()
+
+func show_stc_buttons():
+	$startButton.show()
+	$tutorialButton.show()
+	$creditsButton.show()
+
+func hide_stc_buttons():
 	$startButton.hide()
 	$tutorialButton.hide()
 	$creditsButton.hide()
+
+func show_diffculty_buttons():
+	$easyButton.show()
+	$mediumButton.show()
+	$hardButton.show()
+
+func hide_difficulty_buttons():
+	$easyButton.hide()
+	$mediumButton.hide()
+	$hardButton.hide()
+
+func tempLander():
+	pc = player.instantiate()
+	pc.position = Vector2(600,20)
+	add_child(pc)
 
 #func difficulty():
 #	$easy
