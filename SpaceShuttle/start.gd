a2a2228922543944b08e060107599ade9b21d290
colinmbowser <83146931+colinmbowser@users.noreply.github.com>
2024-11-06 13:19:12 -0700

    Buttons and Background
    Added easy medium and hard buttons
    Added Sprites for background and cockpits (top and bottom)

--- SpaceShuttle/start.gd
+++ SpaceShuttle/start.gd
@@ -5,6 +5,9 @@ var pc
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$easyButton.hide()
+	$mediumButton.hide()
+	$hardButton.hide()
 	pass # Replace with function body.
 
 
@@ -16,24 +19,52 @@ func Play():
 	pass
 
 func _on_startButton_pressed():
+	hide_stc_buttons()
+	$easyButton.show()
+	$mediumButton.show()
+	$hardButton.show()
+
+func _on_tutorial_button_pressed():
+	hide_stc_buttons()
+	#pass
+
+func _on_credits_button_pressed():
+	hide_stc_buttons()
+	#pass
+
+func _on_music_button_pressed():
+	pass # Replace with function body.
+
+func _on_audio_button_pressed():
+	pass # Replace with function body.
+
+func _on_easy_button_pressed():
+	hide_difficulty_buttons()
 	pc = player.instantiate()
 	pc.position = Vector2(20,20)
 	add_child(pc)
-	hide_buttons()
 
-func _on_tutorialButton_pressed():
-	pass
+func _on_medium_button_pressed():
+	hide_difficulty_buttons()
+	pc = player.instantiate()
+	pc.position = Vector2(20,20)
+	add_child(pc)
 
-func _on_creditsButton_pressed():
-	pass
+func _on_hard_button_pressed():
+	hide_difficulty_buttons()
+	pc = player.instantiate()
+	pc.position = Vector2(20,20)
+	add_child(pc)
 
-func _on_settingsButton_pressed():
-	pass
-
-func hide_buttons():
+func hide_stc_buttons():
 	$startButton.hide()
 	$tutorialButton.hide()
 	$creditsButton.hide()
+
+func hide_difficulty_buttons():
+	$easyButton.hide()
+	$mediumButton.hide()
+	$hardButton.hide()
 
 #func difficulty():
 #	$easy
