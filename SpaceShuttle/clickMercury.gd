107bb45323f0ecbbe1fd078219f44df740a4bb22
colinmbowser <83146931+colinmbowser@users.noreply.github.com>
2024-11-12 16:27:22 -0700

    added 10 planets
    each planet reads different

---
+++ SpaceShuttle/clickMercury.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int):
+	if event.is_action_pressed("clickPlanet"):
+		print("Mercury")
