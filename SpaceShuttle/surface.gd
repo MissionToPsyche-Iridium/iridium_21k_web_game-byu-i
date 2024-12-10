0b576a405649f3e74cceb7dec580172cd001b53c
Joseph W. Linton II <91572148+Darkhero789@users.noreply.github.com>
2024-12-09 22:12:21 -0700

    Terrain is colored

--- SpaceShuttle/surface.gd
+++ SpaceShuttle/surface.gd
@@ -13,6 +13,17 @@ var planet = []
 var grC = []
 var lp
 var p
+var mercury = [[101,101,101], [121,121,121]]#hex codes[656565 797979]
+var venus = [[148,83,25], [234,194,107], [182,108,33]] #hex codes [945319 eac26b b66c21]
+var earth = [[143,110,62], [70,95,71], [115, 110, 73]] #hex codes [8f6e3e 465f47 736e49]
+var mars = [[125,88,69], [164,117,91], [86,61,48]] #hex codes [7d5845 a4745b 563d30]
+var psyche = [[181,159,151], [235,203,155], [81,95,105], [141,108,107], [25,30,67], [173,194,240]] #hex codes [b59a97 ebcb9b 515f69 8d6c6b 191e43 adc2f0]
+var jupiter = [[212,178,121], [201,140,75], [183,131,98], [195,186,183], [174,93,44], [143,95,86]] #hex codes [d4b279 c98d4b b78262 c3bab7 ae5d2c 8f5f56]
+var saturn = [[179,164,126], [226,202,158], [161,152,115], [205,181,128], [201,169,122]] #hex codes [b3a47e e2ca9e a19873 cdb580 c9a97a]
+var uranus = [99,187,254] #hex codes [63bcfe]
+var neptune = [[65,110,230], [56,97,200], [153,170,218], [32,54,113]] #hex codes [416ee6 3860c8 99aada 203671]
+var pluto = [[202,167,137], [239,197,162], [120,99,81], [175,144,119], [12,10,8]] #hex codes [caa789 efc5a2 786351 af9077 0c0a08]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -80,12 +91,56 @@ func create_ground(diff, pl):
 	var c = 1
 	@warning_ignore("unused_variable")
 	var pAng
+	var plC
 	for pos in points:
 		#if pos != lpPos:
 		var g = ground.instantiate()
 		g.position = pos
 		add_child(g)
 		planet.append(g)
+		var ri
+		var rc
+		match pl:
+			"Mercury":
+				ri = rng.randi_range(0,1)
+				rc = mercury[ri]
+				plC = getColorPts(rc)
+			"Venus":
+				ri = rng.randi_range(0,2)
+				rc = venus[ri]
+				plC = getColorPts(rc)
+			"Earth":
+				ri = rng.randi_range(0,2)
+				rc = earth[ri]
+				plC = getColorPts(rc)
+			"Mars":
+				ri = rng.randi_range(0,2)
+				rc = mars[ri]
+				plC = getColorPts(rc)
+			"Jupiter":
+				ri = rng.randi_range(0,5)
+				rc = jupiter[ri]
+				plC = getColorPts(rc)
+			"Saturn":
+				ri = rng.randi_range(0,4)
+				rc = saturn[ri]
+				plC = getColorPts(rc)
+			"Uranus":
+				rc = uranus
+				plC = getColorPts(rc)
+			"Neptune":
+				ri = rng.randi_range(0,3)
+				rc = neptune[ri]
+				plC = getColorPts(rc)
+			"Pluto":
+				ri = rng.randi_range(0,4)
+				rc = pluto[ri]
+				plC = getColorPts(rc)
+			"Psyche":
+				ri = rng.randi_range(0,5)
+				rc = psyche[ri]
+				plC = getColorPts(rc)
+		g.setNewColor(plC)
 		var n = points[c]
 		var gPos = g.position
 		@warning_ignore("confusable_local_declaration")
@@ -148,6 +203,7 @@ func create_ground(diff, pl):
 			gC.position = Vector2(i+sx,y)
 			add_child(gC)
 			grC.append(gC)
+			gC.setNewColor(plC)
 			#if memory == null and y != p.y:
 				#pass
 	
@@ -241,3 +297,10 @@ func stars():
 	while x >= minXB & x <= maxXB & y >= minYB & y <= maxYB:
 		x = rng.randi_range(50,1100)
 		y = rng.randi_range(50,600)
+
+func getColorPts(color):
+	var r = color[0]
+	var g = color[1]
+	var b = color[2]
+	var c = Color8(r,g,b)
+	return c
